82bba1c4410fd1f5fb4ff36885093fac301135c4
vladiusftw <61135058+vladiusftw@users.noreply.github.com>
2022-02-16 16:20:16 +0400

    Update Graph.java

--- CSCI203/Assignment2/Graph.java
+++ CSCI203/Assignment2/Graph.java
@@ -1,35 +1,54 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Graph {
-    private Map<Node, ArrayList<Node>> map;
+    private Map<String, ArrayList<String>> map;
 
     public Graph(){
         map = new HashMap<>();
     }
 
-    public Map<Node,ArrayList<Node>> getMap(){return map;}
-    public void setMap(Map<Node,ArrayList<Node>> map){this.map = map;}
+    public Map<String,ArrayList<String>> getMap(){return map;}
+    public void setMap(Map<String,ArrayList<String>> map){this.map = map;}
 
     public void addNode(String label){
-        map.put(new Node(label),new ArrayList<Node>());
+        map.put(label,new ArrayList<>());
     }
 
     public void removeNode(String label){
-        map.remove(new Node(label));
+        map.remove(label);
     }
 
     public void addEdge(String label1,String label2){
-       map.get(new Node(label1)).add(new Node(label2));
+        map.get(label1).add(label2);
     }
 
     public void removeEdge(String label1,String label2){
-        map.get(new Node(label1)).remove(new Node(label2));
+        map.get(label1).remove(label2);
     }
 
-    public boolean BreadthFirstSearch(String label){
+    public ArrayList<String> BreadthFirstSearch(String root){
+        ArrayList<String> visited = new ArrayList<>();
+        visited.add(root);
+        Queue<String> queue = new LinkedList<>(map.get(root));
+        while(queue.size() != 0){
+            String temp = queue.poll();
+            visited.add(temp);
+            queue.addAll(map.get(temp));
+        }
+        return visited;
+    }
 
+    public ArrayList<String> DepthFirstSearch(String root){
+        ArrayList<String> visited = new ArrayList<>();
+        visited.add(root);
+        Stack<String> stack = new Stack<>();
+        stack.addAll(map.get(root));
+        while(stack.size() != 0){
+            String temp = stack.pop();
+            visited.add(temp);
+            stack.addAll(map.get(temp));
+        }
+        return visited;
     }
 
     public String toString(){
